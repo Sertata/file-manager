db3d2ea6e95a74775ed3ec2c23fe2f7b0d574190
StrelkovSS <sergeistrelkov98@gmail.com>
2023-10-20 20:33:10 +0300

    Draft 2033 20102023

--- src/pages/FileManager/FileViewer.tsx
+++ src/pages/FileManager/FileViewer.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react';
 
 interface FileViewerProps {
-  onWatchClick: () => void;
+  onDialogAction: () => void;
+  open: boolean;
 }
 
 export const FileViewer: FC<FileViewerProps> = (props) => {
-  const { onWatchClick } = props;
+  const { onDialogAction, open } = props;
   return (
     <div>
-      <button onClick={onWatchClick}>Watch</button>
+      <button onClick={onDialogAction}>{`${open ? 'Close' : 'Watch'}`}</button>
+      <dialog>
+        <iframe />
+      </dialog>
     </div>
   );
 };
--- src/pages/FileManager/index.tsx
+++ src/pages/FileManager/index.tsx
@@ -1,17 +1,32 @@
-import { ChangeEvent, FC, useState } from 'react';
+import { ChangeEvent, FC, useState, useEffect } from 'react';
 import { FileInput } from './FileInput';
+import { FileViewer } from './FileViewer';
 
 const FileManager: FC = () => {
   const [file, setFile] = useState<File>();
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState('');
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
     }
   };
+  const handleDialogAction = () => setOpen((prev) => !prev);
+
+  useEffect(() => {
+    if (open && file) {
+      toBase64(file)
+        .then((base64String) => {
+          console.log(base64String);
+        })
+        .catch((e) => console.error(e));
+    }
+  }, [open, file]);
   return (
     <div>
       <FileInput file={file} onFileChange={handleFileChange} />
+      <FileViewer onDialogAction={handleDialogAction} open={open} />
     </div>
   );
 };
